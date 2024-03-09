30e0fe44a3939d5e645da23f4c883d4a6fba87c5
JoDoFa <JoDoFa>
2024-03-09 13:59:15 +0800

    My first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+   Joel Fajardo Jr . D
+   <p>ADET BSIT - II</p>
+
+    </head>
+    <body>
+    <?php
+    
+    $x = 20;  
+    $x += 100;
+    
+    echo $x;
+    
+    $x = 50;
+    $x -= 30;
+    
+    echo $x;
+    
+    $x = 5;
+    $x *= 6;
+    
+    echo $x;
+    
+    $x = 10;
+    $x /= 5;
+    
+    echo $x;
+    
+    $x = 15;
+    $x %= 4;
+    
+    echo $x;
+?>  
+</body>
+</html>  
