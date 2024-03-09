d5e9e9138ec1b15e4b6921fb923400608722a344
JoDoFa <JoDoFa>
2024-03-09 15:31:53 +0800

    addition

---
+++ Math.php
@@ -0,0 +1,12 @@
+<?php
+$input1 = $_POST["Input1"];
+$input2 = $_POST["Input2"];
+
+
+
+$output = 0;
+$output = $input1 + $input2;
+
+echo "The sum is: ".$output;
+
+?>
--- index.php
+++ index.php
@@ -5,32 +5,12 @@
 
     </head>
     <body>
-    <?php
-    
-    $x = 20;  
-    $x += 100;
-    
-    echo $x;
-    
-    $x = 50;
-    $x -= 30;
-    
-    echo $x;
-    
-    $x = 5;
-    $x *= 6;
-    
-    echo $x;
-    
-    $x = 10;
-    $x /= 5;
-    
-    echo $x;
-    
-    $x = 15;
-    $x %= 4;
-    
-    echo $x;
-?>  
+   
+    <form action="Math.php" method="post">
+    Input1: <input type="text" name="Input1"><br>
+    Input2: <input type="text" name="Input2"><br>
+    <input type="submit">
+    </form>
+     
 </body>
 </html>  
