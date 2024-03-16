09c7828c418588f7589bc08b60d2ba1d1ee5ceba
JoDoFa <JoDoFa>
2024-03-16 14:27:33 +0800

    Math

--- Math.php
+++ Math.php
@@ -1,12 +1,24 @@
 <?php
-$input1 = $_POST["Input1"];
-$input2 = $_POST["Input2"];
+$input1 = $_GET["Input1"];
+$input2 = $_GET["Input2"];
+$Math = $_GET["Math"];
+$output =0;
+
+echo $Math."<br>";
 
 
-
-$output = 0;
-$output = $input1 + $input2;
-
-echo "The sum is: ".$output;
+if($Math =="add"){
+    $output = $input1 + $input2;
+    echo "output is: " .$output;
+}elseif($Math =="minus"){
+    $output = $input1 - $input2 ;
+    echo "The output is: ".$output;
+}elseif($Math =="multiply"){
+    $output = $input1 * $input2 ;
+    echo "The output is: ".$output;
+}elseif($Math =="divide"){
+    $output = $input1 / $input2 ;
+    echo "The output is: ".$output;
+}
 
 ?>
--- index.php
+++ index.php
@@ -6,8 +6,16 @@
     </head>
     <body>
    
-    <form action="Math.php" method="post">
+    <form action="Math.php" method="GET">
     Input1: <input type="text" name="Input1"><br>
+    Select: </S><select name="Math">
+        <option value="add">+</option>
+        <option value="minus">-</option>
+        <option value="multiply">*</option>
+        <option value="divide">/</option>
+    <Select>
+    <br>
+
     Input2: <input type="text" name="Input2"><br>
     <input type="submit">
     </form>
